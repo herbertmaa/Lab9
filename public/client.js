3036eb3b99d23e9bf169a070d56913a0bae7d925
herbertmaa <hma49@sfu.ca>
2019-04-03 18:52:57 -0700

    Added Functionality

--- public/client.js
+++ public/client.js
@@ -1,40 +1,56 @@
-
 /** This method calls from our server and loads the carousel based on the movies playing. It automatically updates the carousel when this information is changed **/
-function callHTML() {
+function callHTML(number) {
     // Request for .HTML file
 
     $.ajax({
 
         type: "get",
-        url: "/term3.html",
+        url: ("/term" + number + ".html"),
         success: function (data) {
-   
-            console.log(data);
 
+            var element = '#term' + number + 'descript ' + '.card';
+            $(element).html(" ");
+            $(element).append(data);
+        }
+
+    });
+
+}
+
+
+function callJSON(number) {
+
+    // Request for .JSON file
+    $.ajax({
+
+        type: "get",
+        url: ("/term" + number + ".json"),
+        success: function (data) {
+            var element = '#term' + number + 'descript ' + '.card';
+            $(element).html(" ");
+            var myDivArray = parseJSON2HTML(data);
+            for (var i = 0; myDivArray.length > i; i++) {
+                $(element).append(myDivArray[i]);
+            }
         }
 
     });
 
 
-
-
 }
 
 
-function callJSON(){
-    
-     // Request for .JSON file
-        $.ajax({
+function parseJSON2HTML(myJSON) {
 
-            type: "get",
-            url: "/movies.json",
-            success: function (data) {
+    var divArray = [];
+    for (var i = 0; i < myJSON.length; i++) {
+        var div = $('<div> </div>');
+        var h6 = $('<h6>' + myJSON[i].course_credits + '</h6');
+        var h4 = $('<h4>' + myJSON[i].course_name + '</h4>');
+        var h5 = $('<h5>' + myJSON[i].course_id + '</h5>');
+        div.append(h4).append(h5).append(h6);
+        divArray.push(div);
+    }
 
-                console.log(data);
-            }
-
-        });
-    
-    
-    
+    return divArray;
 }
